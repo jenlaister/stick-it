f60585fdbc003da30b654bb0fa22cd87fe62b6d8
Jenny Lai <jenny.lai@flatironschool.com>
2017-03-24 18:31:01 -0400

    working on schema

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,5 +4,7 @@ class User < ApplicationRecord
 
   has_many :routines
   has_many :habits, through: :routines
+  has_many :progresses, through: :routines
+  has_many :notes, through: :routines
 
 end
